7d3b2ff1432b0cf24c03814865525cb7572f3e63
davinerd <anathema@anche.no>
2018-06-19 15:15:45 +0200

    added ossec wazuh

---
+++ ossec_wazuh/example/data.tf
@@ -0,0 +1,35 @@
+data "template_file" "s3_bucket_name" {
+    template = "$${bucket_name}"
+
+    vars {
+      bucket_name = "wazuh-${var.wazuh_name}"
+    }
+}
+
+data "aws_ami" "amzn_ossec_ami" {
+  most_recent = true
+
+  filter {
+    name   = "tag-value"
+    values = ["ossec"]
+  }
+
+  filter {
+    name   = "tag-key"
+    values = ["Service"]
+  }
+
+  filter {
+    name   = "tag-value"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "tag-key"
+    values = ["latestAmi"]
+  }
+}
+
+data "vault_generic_secret" "wazuh_creds" {
+  path = "secret/wazuh_creds/test"
+}
